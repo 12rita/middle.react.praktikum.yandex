25e28e370b868ca63f2b8d360a523476ced681ba
ma.k.goncharova <ma.k.goncharova@tbank.ru>
2025-03-06 23:33:19 +0300

    feat(sprint-5): in progress

---
+++ src/pages/Orders/mock.ts
@@ -0,0 +1,80 @@
+export const orders = [
+  {
+    id: "23B6EB0D-8114-D298-9671-2DABB86512B9",
+    name: "Dacey Weeks",
+    ingredients: [
+      "643d69a5c3f7b9001cfa093f",
+      "643d69a5c3f7b9001cfa093d",
+      "643d69a5c3f7b9001cfa093c",
+      "643d69a5c3f7b9001cfa093f",
+      "643d69a5c3f7b9001cfa0944",
+    ],
+    price: 554.97,
+    timestamp: "Feb 26, 2026",
+  },
+  {
+    id: "439B48C1-2310-D27E-253C-C9DA4873C1BF",
+    name: "Mary Pierce",
+    ingredients: ["643d69a5c3f7b9001cfa0945", "643d69a5c3f7b9001cfa0946"],
+    price: 862.74,
+    timestamp: "Jun 16, 2025",
+  },
+  {
+    id: "A18CD252-3767-B126-8D82-23884894B683",
+    name: "Dawn Figueroa",
+    ingredients: [
+      "643d69a5c3f7b9001cfa093f",
+      "643d69a5c3f7b9001cfa0947",
+      "643d69a5c3f7b9001cfa093c",
+      "643d69a5c3f7b9001cfa093c",
+    ],
+    price: 885.5,
+    timestamp: "Aug 21, 2025",
+  },
+  {
+    id: "62B538A7-D9EE-A568-3379-E92E9AF82286",
+    name: "Kelly Johnston",
+    ingredients: [
+      "643d69a5c3f7b9001cfa094a",
+      "643d69a5c3f7b9001cfa0942",
+      "643d69a5c3f7b9001cfa0941",
+      "643d69a5c3f7b9001cfa0940",
+      "643d69a5c3f7b9001cfa0947",
+      "643d69a5c3f7b9001cfa093c",
+      "643d69a5c3f7b9001cfa0945",
+      "643d69a5c3f7b9001cfa0941",
+    ],
+    price: 344.44,
+    timestamp: "Sep 1, 2024",
+  },
+  {
+    id: "3EDD268E-C694-D2E8-DB66-BCDCF4B7AC50",
+    name: "Luke Higgins",
+    ingredients: [
+      "643d69a5c3f7b9001cfa0948",
+      "643d69a5c3f7b9001cfa093d",
+      "643d69a5c3f7b9001cfa093c",
+      "643d69a5c3f7b9001cfa093c",
+      "643d69a5c3f7b9001cfa0947",
+      "643d69a5c3f7b9001cfa094a",
+      "643d69a5c3f7b9001cfa093d",
+      "643d69a5c3f7b9001cfa093d",
+      "643d69a5c3f7b9001cfa093c",
+    ],
+    price: 347.89,
+    timestamp: "Jul 6, 2025",
+  },
+];
+
+const stats = {
+  ready: [
+    "23B6EB0D-8114-D298-9671-2DABB86512B9",
+    "3EDD268E-C694-D2E8-DB66-BCDCF4B7AC50",
+  ],
+  in_progress: [
+    "62B538A7-D9EE-A568-3379-E92E9AF82286",
+    "439B48C1-2310-D27E-253C-C9DA4873C1BF",
+  ],
+  all_time: 28752,
+  today: 138,
+};
